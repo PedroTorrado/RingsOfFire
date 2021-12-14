9ec12b2b1657bc3d9d9317333785762fb3f44a41
PedroTorrado <pedro.espanha.torrado@gmail.com>
2021-12-14 11:37:05 +0000

    Changed way of defining suits

--- RingsOfFire_GUI.py
+++ RingsOfFire_GUI.py
@@ -1,30 +1,29 @@
 import random
 from PySimpleGUI import PySimpleGUI as sg
-import time
 
-clubs = ["Ace of clubs", "2 of clubs", "3 of clubs",
-       "4 of clubs", "5 of clubs", "6 of clubs",
-       "7 of clubs", "8 of clubs", "9 of clubs",
-       "10 of clubs", "Jack of clubs",
-       "Queen of clubs", "King of clubs"]
+figures = ["Jack", "Queen", "King", "Ace"]
+numbers = list(range(2, 11))
+numbers_str = list(map(str, numbers))
+cards = numbers_str + figures
 
-spades = ["Ace of spades", "2 of spades", "3 of spades",
-        "4 of spades", "5 of spades", "6 of spades",
-        "7 of spades", "8 of spades", "9 of spades",
-        "10 of spades", "Jack of spades",
-        "Queen of spades", "King of spades"]
+hearts_name = " of Hearts"
+diamonds_name = " of Diamonds"
+spades_name = " of Spades"
+clubs_name = " of Clubs"
 
-diamonds = ["Ace of diamonds", "2 of diamonds", "3 of diamonds",
-          "4 of diamonds", "5 of diamonds", "6 of diamonds",
-          "7 of diamonds", "8 of diamonds", "9 of diamonds",
-          "10 of diamonds", "Jack of diamonds",
-          "Queen of diamonds", "King of diamonds"]
+hearts = ["","","","","","","","","","","","",""]
+diamonds = ["","","","","","","","","","","","",""]
+spades = ["","","","","","","","","","","","",""]
+clubs = ["","","","","","","","","","","","",""]
 
-hearts = ["Ace of hearts", "2 of hearts", "3 of hearts",
-        "4 of hearts", "5 of hearts", "6 of hearts",
-        "7 of hearts", "8 of hearts", "9 of hearts",
-        "10 of hearts", "Jack of hearts",
-        "Queen of hearts", "King of hearts"]
+def suit(suits, suit_name):
+    for i in range(len(cards)):
+        suits[i] = cards[i] + suit_name
+
+suit(hearts, hearts_name)
+suit(diamonds, diamonds_name)
+suit(spades, spades_name)
+suit(clubs, clubs_name)
 
 Jokers = ["Joker", "Joker"]
 
