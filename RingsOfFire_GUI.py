af7335894f549527e57098d2cc1ffbe1b8143be5
PedroTorrado <pedro.espanha.torrado@gmail.com>
2021-12-20 23:18:47 +0000

    Update RingsOfFire_GUI.py

--- RingsOfFire_GUI.py
+++ RingsOfFire_GUI.py
@@ -5,26 +5,22 @@ figures = ["Jack", "Queen", "King", "Ace"]
 numbers = list(range(2, 11))
 numbers_str = list(map(str, numbers))
 cards = numbers_str + figures
- 
-# defining the names of the suits to be added to each item in the different lists
-hearts_name = " of Hearts"
+
+hearts_name = " of Hearts"                                      # defining the names of the suits to be added to each item in the different lists
 diamonds_name = " of Diamonds"
 spades_name = " of Spades"
 clubs_name = " of Clubs"
 
-# defining each suit so items can be added to them
-hearts = ["","","","","","","","","","","","",""]
+hearts = ["","","","","","","","","","","","",""]               # defining each suit so items can be added to them
 diamonds = ["","","","","","","","","","","","",""]
 spades = ["","","","","","","","","","","","",""]
 clubs = ["","","","","","","","","","","","",""]
 
-# function that adds the suit name to each item of the list (and card)
-def suit(suits, suit_name):
+def suit(suits, suit_name):                                     # function that adds the suit name to each item of the list (and card)
     for i in range(len(cards)):
         suits[i] = cards[i] + suit_name
 
-# use of the suit funtion so each element has a card value followed by it's suit
-suit(hearts, hearts_name)
+suit(hearts, hearts_name)                                       # use of the suit funtion so each element has a card value followed by it's suit
 suit(diamonds, diamonds_name)
 suit(spades, spades_name)
 suit(clubs, clubs_name)
@@ -34,20 +30,18 @@ Jokers = ["Joker", "Joker"]
 blackSuits = clubs + spades
 redSuits = diamonds + hearts
 
-#defining a deck as the totality of all the suits (and jokers)
-deck = blackSuits + redSuits
+deck = blackSuits + redSuits                                    # defining a deck as the totality of all the suits (and jokers)
 
 sg.theme("DarkAmber")
 
-# funtion center created to facilitate the use of funtions in the following format:
-# sg.Stretch(), sg.Text(" "), sg.Stretch()
-def center(item):
-    return [sg.Stretch(), *item, sg.Stretch()]
+def center(item):                                               # funtion center created to facilitate the use of funtions in the following format:
+    return [sg.Stretch(), *item, sg.Stretch()]                  # sg.Stretch(), sg.Text(" "), sg.Stretch()
 
-# 1st screen shown to player, asks if jokers are to be included and prompts the user to start the game
+
+                                                                # 1st screen shown to player, asks if jokers are to be included and prompts the user to start the game
 def WarmUp():
     layout = [
-        #defining different objects from PySimpleGUI to use on the first window
+                                                                #defining different objects from PySimpleGUI to use on the first window
         center([sg.Text("--- Welcome to Rings of Fire ---")]),  # Top Text
         center([sg.Text("Do you wish to include Jokers?")]),    # Under text question
         center([sg.Button("Jokers")]),                          # Button Text
@@ -87,9 +81,9 @@ def Game():
         size = (275, 200),
         finalize = True)
 
-window1, window2 = WarmUp(), None   # defining two windows and declaring the WarmUp() funtion as window1
+window1, window2 = WarmUp(), None                                       # defining two windows and declaring the WarmUp() funtion as window1
 
-jokerstate = 0                      # defining a the state of jokers as non included to later be used
+jokerstate = 0                                                          # defining a the state of jokers as non included to later be used
 print("Jokers won't be included")
 
 while True:
@@ -118,9 +112,9 @@ while True:
         window2 = Game()                                                # Window2 (Game() window) is opened
 
         if jokerstate == 0:                                             # if jokerstate is zero it means the jokers shouldn't be included in the deck
-             deck = deck                                                # so that jokers aren't included the list deck is equal to itself 
+             deck = deck                                                # so that jokers aren't included the list deck is equal to itself
                                                                         # this step isn't needed but makes it easier to compare the two if statements and pratically changes nothing
-             print("Cards on a deck:", len(deck))                       # prints to CMD the amount of cards on a deck 
+             print("Cards on a deck:", len(deck))                       # prints to CMD the amount of cards on a deck
 
         if jokerstate == 1:                                             # if jokerstate is one ir means the jokers should be included in the deck
             deck = deck + Jokers                                        # to include jokers the jokers list is added to the deck list making it a 54 item list
@@ -133,7 +127,7 @@ while True:
     if window == window2 and event == "RESTART_BUTTON":                 # if the restart button is pressed
                                                                         # the jokerstate is checked to see which deck was being used, a 52 card or a 54
         if jokerstate == 0: deck = blackSuits + redSuits                # since the items of the deck list where removed this needs to be recomposed by the lists from the biggining
-        if jokerstate == 1: deck = blackSuits + redSuits + Jokers       # same here but including also the jokers 
+        if jokerstate == 1: deck = blackSuits + redSuits + Jokers       # same here but including also the jokers
         print("\n********* Game Restarted *********")
         window["NUMBER_DISPLAY"].update(                                # the display which shows the number of cards in a deck gets updated to show the deck is full
             value = f"Number of cards on the deck: {len(deck)}")
@@ -151,7 +145,7 @@ while True:
         if card in blackSuits:                                          # if the card belongs to one of the black suits it's shown in black lettering
             window["YOUR_CARD"].update(text_color = "Black")
         elif card in redSuits:                                          # if the card belongs to one of the red suits it's shown in red lettering
-            window["YOUR_CARD"].update(text_color = "Red")              
+            window["YOUR_CARD"].update(text_color = "Red")
         else:                                                           # if the card is a Joker it's shown in green lettering
             window["YOUR_CARD"].update(text_color = "Green")
 
